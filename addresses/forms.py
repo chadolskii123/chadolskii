0b4e130dc15d194db5c3b7ffd0e431956f1d5cc5
chadolskii123 <chadolskii123@gmail.com>
2020-11-17 18:02:25 +0900

    change Kor for shipping address

--- addresses/forms.py
+++ addresses/forms.py
@@ -9,10 +9,20 @@ class AddressForm(forms.ModelForm):
         fields = [
             #'billing_profile',
             #'address_type',
-            'address_line_1',
-            'address_line_2',
-            'city',
             'country',
             'state',
+            'city',
+            'address_line_1',
+            'address_line_2',
             'postal_code',
         ]
+        labels = {
+            # 'billing_profile',
+            # 'address_type',
+            'country': "시",
+            'state': "구",
+            'city': "동",
+            'address_line_1' : "주소 1",
+            'address_line_2': "주소 2",
+            'postal_code':"우편번호",
+        }
