15f154fdf9631ff346347f75faa5572af5ad536e
chadolskii123 <chadolskii123@gmail.com>
2020-12-18 10:57:04 +0900

    change chimp

--- ecomm/settings/production.py
+++ ecomm/settings/production.py
@@ -34,7 +34,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'chadolskii123@gmail.com'
-EMAIL_HOST_PASSWORD = 'tpkclgmdpowmyegb'
+EMAIL_HOST_PASSWORD = 'nhgkolavslwbeijx'
 SERVER_EMAIL = 'chadolskii123@gmail.com'
 DEFAULT_FROM_MAIL = 'chadolskii123'
 
